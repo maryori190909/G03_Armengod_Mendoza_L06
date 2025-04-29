18e0b8a639bcecf851c30566333b070a1747114e
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-29 15:49:21 -0500

    v3

--- Actividad_01/Tarea.java
+++ Actividad_01/Tarea.java
@@ -2,4 +2,9 @@ public class Tarea {
     private String titulo;
     private int prioridad;
 
+    public Tarea(String titulo, int prioridad){
+        this.titulo= titulo;
+        this.prioridad = prioridad;
+    }
+
 }
