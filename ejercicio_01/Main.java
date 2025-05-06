27d9dabc663568563ef44cc13ba5992d56dc6722
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-06 01:40:18 -0500

    v3

---
+++ ejercicio_01/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        Gestordetareas <Tarea> gestor = new Gestordetareas<>();
+
+        Tarea t1 = new Tarea(" envio de informe", 4);
+        Tarea t2 = new Tarea(" presentar reporte", 3);
+        Tarea t3 = new Tarea("revisar codigo ", 2);
+
+        gestor.agregarTarea(t1);
+        gestor.agregarTarea(t2);
+        gestor.agregarTarea(t3);
+
+        gestor.eliminarTarea(t3);
+
+        System.out.println(" tareas actuales: ");
+        gestor.imprimirTarea();
+
+        System.out.println(" esta el envio de informe? "+ gestor.contieneTarea(t1));
+
+        gestor.invertirTarea();
+        System.out.println(" tareas invertidas: ");
+        gestor.imprimirTarea();
+
+        gestor.transferiratareascompletas(t2);
+
+        System.out.println(" Tareas actuales: ");
+        gestor.imprimirTarea();
+        gestor.impimirTareasCompletas();
+
+    }
+
+}
