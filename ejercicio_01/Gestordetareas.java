e08320dc0021badfc6fbfdb521e2884a94bf4086
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-06 02:07:03 -0500

    v15

--- ejercicio_01/Gestordetareas.java
+++ ejercicio_01/Gestordetareas.java
@@ -116,8 +116,13 @@ public class Gestordetareas <T> {
 
     public static<T> boolean soniguales (Node<T> a, Node<T> b){
         while(a!= null && b!= null){
-            
+            if (!a.getData().equals(b.getData())){
+                return false
+            }
+            a = a.getNext();
+            b = b.getNext();
         }
+        return a == null && b == null;
     }
 
 }
