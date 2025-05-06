a37aa43a6e954d08bcc4b69765042562601478ab
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-05-06 01:09:14 -0500

    v35

--- Actividad_01/Main.java
+++ Actividad_01/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Gestordetareas <Tarea> gestor = new Gestordetareas<>();
+
+        Tarea t1 = new Tarea(" envio de informe", 4);
+        Tarea t2 = new Tarea(" presentar reporte", 3);
+        Tarea t3 = new Tarea("revisar codigo ", 2);
+
         
     }
 
